2264410c61a8f6e12b5da18c265a4f75558bfbbe
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-10-09 22:20:40 +0800

    Add assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  //C:\Users\Hao Xiang\BCC_Web_Dev_V2\public\assets\main.css
-  css: ['/assets/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  devtools: { enabled: true }
 })
