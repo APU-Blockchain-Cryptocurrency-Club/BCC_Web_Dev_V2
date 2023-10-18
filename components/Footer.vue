2bf52b02ce500325babfb8aba8e86f0666d05ae3
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-10-18 16:05:53 +0800

    Add Daisy Components to Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,98 +1,68 @@
 <template>
-    <div class="flex bg-slate-600 py-10 px-32">
-        
-            <div class="grid">
-                <div>
-                    <nuxt-link to="/">
-                        <img src="../assets/APUBCC Logo.png" alt="APU BCC Logo" width="200">
-                    </nuxt-link>
-                </div>
-
-                <div class="flex m-2">
-
-                    <a href="https://www.facebook.com/apubcc/" target="_blank" class="hover:bg-black transition delay-600 b rounded-full">
-                        <img src="../assets/white fb.png" alt="APUBCC Facebook" width="35" class="m-1">
-                    </a>
-
-                    <a href="https://twitter.com/apubcc" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
-                        <img src="../assets/white twitter.png" alt="APUBCC Twitter" width="35" class="m-1">
-                    </a>
-
-                    <a href="https://my.linkedin.com/company/apu-blockchain-crypto-club-apu-bcc?original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
-                        <img src="../assets/white linked.png" alt="APUBCC LinkedIn" width="35" class="m-1">
-                    </a>
-
-                    <a href="https://www.instagram.com/apubcc/?hl=en" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
-                        <img src="../assets/white ig.png" alt="APUBCC Instagram" width="35" class="m-1">
-                    </a>
-
-                    <a href="https://www.youtube.com/channel/UC0WUwwcvh_A7to7YwoK-mrw" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
-                        <img src="../assets/white you.png" alt="APUBCC Youtube" width="35" class="m-1">
-                    </a>
-
-                    <a href="https://github.com/APU-Blockchain-Cryptocurrency-Club" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
-                        <img src="../assets/white gh.png" alt="APUBCC GitHub" width="35" class="m-1">
-                    </a>
-
-                </div>
-                <div class="text-xs pl-3 text-white">
-                    <p>@APUBCC 2023</p>
-                </div>
+    <footer class="footer p-10 bg-slate-600 text-base-content pl-32">
+        <aside>
+            <div>
+                <nuxt-link to="/">
+                    <img src="../assets/APUBCC Logo.png" alt="APU BCC Logo" width="200">
+                </nuxt-link>
             </div>
-            <div class="flex mx-auto">
-                <div class="footer-block text-left mr-20" >
-                    <b class="footer-heading text-base text-orange-400">General</b>
-                    <ul class="footer-components">  
 
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer">About Us</li>
+            <div class="flex m-2">
 
+                <a href="https://www.facebook.com/apubcc" target="_blank" class="hover:bg-black transition delay-600 b rounded-full">
+                    <img src="../assets/white fb.png" alt="APUBCC Facebook" width="35" class="m-1">
+                </a>
 
+                <a href="https://twitter.com/apubcc" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
+                    <img src="../assets/white twitter.png" alt="APUBCC Twitter" width="35" class="m-1">
+                </a>
 
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer">Our Team </li>
+                <a href="https://www.linkedin.com/company/apu-blockchain-crypto-club-apu-bcc" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
+                    <img src="../assets/white linked.png" alt="APUBCC LinkedIn" width="35" class="m-1">
+                </a>
 
-                        
+                <a href="https://www.instagram.com/apubcc" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
+                    <img src="../assets/white ig.png" alt="APUBCC Instagram" width="35" class="m-1">
+                </a>
 
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer">FAQ</li>
+                <a href="https://www.youtube.com/@apubcc" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
+                    <img src="../assets/white you.png" alt="APUBCC Youtube" width="35" class="m-1">
+                </a>
 
-                    </ul>
-                </div>
+                <a href="https://github.com/APU-Blockchain-Cryptocurrency-Club" target="_blank" class="hover:bg-black transition delay-600 b b rounded-full">
+                    <img src="../assets/white gh.png" alt="APUBCC GitHub" width="35" class="m-1">
+                </a>
 
-                <div class="footer-block text-left mr-20">
-                    <b class="footer-heading text-base text-orange-400">Event</b>
-                    <ul class="footer-components">
-
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer"> Events </li>
-
-                    </ul>
-                </div>
-
-                <div class="footer-block text-left mr-20">
-                    <b class="footer-heading text-base text-orange-400"> Resources </b>
-                    <ul class="footer-components">
-
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer"> Blog </li>
-
-                        
-
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer">Media Kit</li>
-
-                        
-                    </ul>
-                </div>
-
-                <div class="footer-block text-left mr-20">
-                    <b class="footer-heading text-base text-orange-400"> Support Us </b>
-                    <ul class="footer-components">
-                        
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer">Buy Us coffee</li>
-
-                    
-
-                            <li class="text-white hover:text-orange-400 transition delay-600 cursor-pointer">Crypto Addresses</li>
-                        
-                    </ul>
-                </div>
             </div>
-    </div>
+            <div class="text-xs pl-3 text-white">
+                <p>@APUBCC 2023</p>
+            </div>
 
+        </aside> 
+        <nav>
+            <header class="font-bold text-base text-orange-400"> General </header> 
+            <a class="link link-hover text-white"> About Us</a> 
+            <a class="link link-hover text-white">Our Team</a>
+            <nuxt-link to="/faq">
+                <a class="link link-hover text-white">FAQ</a>
+            </nuxt-link>
+
+        </nav> 
+        <nav>
+            <header class="font-bold text-base text-orange-400"> Event </header> 
+            <nuxt-link to="/events">
+                <a class="link link-hover text-white">Events</a>
+            </nuxt-link>
+        </nav> 
+        <nav>
+            <header class="font-bold text-base text-orange-400"> Resources </header> 
+            <a class="link link-hover text-white">Blog</a>
+            <a class="link link-hover text-white">Media Kit</a>
+        </nav>
+        <nav>
+            <header class="font-bold text-base text-orange-400"> Support Us </header> 
+            <a class="link link-hover text-white">Buy Us a coffee</a>
+            <a class="link link-hover text-white">Crypto Addresses</a>
+        </nav>
+    </footer>
 </template>
