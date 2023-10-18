1ccf0147b3ed297050d52c46ae3183ba4f51e22b
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-10-18 16:39:10 +0800

    update font size

--- pages/visionmission.vue
+++ pages/visionmission.vue
@@ -10,7 +10,7 @@
       </div>
       
       <div class="p-5 bg-white bg-opacity-25 rounded-3xl">
-        <b class="text-white text-2xl leading-normal"> To be the leading student hub in Malaysia that gather Web3 talents and empower students to contribute their unique skills sets and creativity to the growth of the blockchain ecosystem locally and globally. A progressive platform where students ideas are exchanged, visions are shared and projects are supported and nurtured by various means.</b>
+        <b class="text-white text-lg leading-normal"> To be the leading student hub in Malaysia that gather Web3 talents and empower students to contribute their unique skills sets and creativity to the growth of the blockchain ecosystem locally and globally. A progressive platform where students ideas are exchanged, visions are shared and projects are supported and nurtured by various means.</b>
       </div>
     </div>
 
@@ -20,7 +20,7 @@
       </div>
 
       <div class="bg-white bg-opacity-25 p-5 rounded-3xl">
-        <b class="text-white text-2xl leading-normal"> To establish a community platform that offers blockchain education, support and mentorship that eventually leads to creating more potential use cases of blockchain technology. We are building a community with diverse course backgrounds and connecting industry partners with students to open more doors. We aim to implement practical learning, build significant projects together and educate members with "Gen Z style" of modules.</b>
+        <b class="text-white text-lg leading-normal"> To establish a community platform that offers blockchain education, support and mentorship that eventually leads to creating more potential use cases of blockchain technology. We are building a community with diverse course backgrounds and connecting industry partners with students to open more doors. We aim to implement practical learning, build significant projects together and educate members with "Gen Z style" of modules.</b>
       </div>
     </div>
   </div>
