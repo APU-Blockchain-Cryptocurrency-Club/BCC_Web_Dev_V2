f3cd9031d80699970bcff27f2fb5d9e0fd3a9cbe
AaronTan11 <tanweihup@gmail.com>
2023-10-18 17:23:09 +0800

    initial events page done

---
+++ components/UpComingEvents.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="carousel carousel-center max-w-7xl p-4 space-x-4 bg-neutral rounded-box mb-10"
+        v-for="data in results"
+        :key="data.id"
+    >
+        <div class="carousel-item" v-for="event in data">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img
+                        :src="event.imageUrl"
+                        :alt="event.title"
+                        class="rounded-box w-fit"
+                    />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        {{ event.title }}
+                    </h2>
+                    <p>{{ event.date }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const { data } = await useFetch("/api/fetchUpComingEvents");
+const results = ref(data);
+</script>
--- pages/events.vue
+++ pages/events.vue
@@ -1,3 +1,17 @@
 <template>
-    <p> Test </p>
+    <div class="flex items-center flex-col">
+        <section>
+            <h1 class="text-3xl text-orange-400 font-bold">Up Coming</h1>
+            <div class="w-full">
+                <UpComingEvents />
+            </div>
+        </section>
+
+        <section>
+            <h1 class="text-3xl text-orange-400 font-bold">Past Events</h1>
+            <div class="w-full">
+                <PastEvents />
+            </div>
+        </section>
+    </div>
 </template>
