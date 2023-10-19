a7ba5b50c0388b4fbeb99201c2c8ab58ad4ca805
AaronTan11 <tanweihup@gmail.com>
2023-10-19 20:31:57 +0800

    added arrow buttons to navigate carousel

--- pages/events.vue
+++ pages/events.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="flex items-center flex-col">
-        <section>
-            <h1 class="text-3xl text-orange-400 font-bold">Up Coming</h1>
-            <div class="w-full">
-                <UpComingEvents />
-            </div>
-        </section>
+   <div class="flex items-center flex-col">
+      <section>
+         <h1 class="text-3xl text-orange-400 font-bold">Up Coming</h1>
+         <div class="w-full">
+            <UpComingEvents />
+         </div>
+      </section>
 
-        <section>
-            <h1 class="text-3xl text-orange-400 font-bold">Past Events</h1>
-            <div class="w-full">
-                <PastEvents />
-            </div>
-        </section>
-    </div>
+      <section>
+         <h1 class="text-3xl text-orange-400 font-bold">Past Events</h1>
+         <div class="w-full">
+            <PastEvents />
+         </div>
+      </section>
+   </div>
 </template>
