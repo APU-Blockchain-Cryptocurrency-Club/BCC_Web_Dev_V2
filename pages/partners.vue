92aff540d6292a8f5ef5ce828bc14f9087cf6833
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-10-20 00:01:12 +0800

    done partnership page
    currently is hardcoded

--- pages/partners.vue
+++ pages/partners.vue
@@ -1,3 +1,79 @@
 <template>
-    
+  <div class="pt-32 px-32 pb-8">
+    <!-- top session -->
+    <div class="items-center text-justify">
+      <b class="text-6xl text-orange-400 leading-normal">Partners</b>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+    <div class="items-center text-center">
+      <b class="w-full text-6xl text-white leading-normal"
+        >More partnerships. More opportunities.</b
+      >
+      <p class="w-full text-4xl text-white leading-normal">
+        We collaborate with different company to expand the community.
+      </p>
+    </div>
+
+    <!-- partner list title -->
+    <div class="items-center flex text-center pt-32">
+      <b class="w-full text-6xl text-white leading-normal">Our Partners</b>
+    </div>
+
+    <!-- Partner List(hardcode) -->
+    <div class="flex pt-8">
+      <div class="mr-8 w-4/12 card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Partner name</h2>
+          <p>Description</p>
+        </div>
+      </div>
+
+      <div class="mr-8 w-4/12 card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Partner name</h2>
+          <p>Description</p>
+        </div>
+      </div>
+      <div class="mr-8 w-4/12 card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Partner name</h2>
+          <p>Description</p>
+        </div>
+      </div>
+      <div class="mr-8 w-4/12 card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Partner name</h2>
+          <p>Description</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
