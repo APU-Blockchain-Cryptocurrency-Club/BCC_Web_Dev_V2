add1bf67f99c42b74933858c819832017afe70ac
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-10-22 16:35:01 +0800

    add history page content

--- pages/history.vue
+++ pages/history.vue
@@ -1,3 +1,16 @@
 <template>
-    
+    <div class="p-32 pt-12 pb-12">
+        <div>
+            <b class="text-6xl text-orange-400 leading-normal"> History </b>
+        </div>
+        <ul class="steps steps-vertical">
+            <li data-content="?" class="step step-neutral">Step 1</li>
+            <li data-content="!" class="step step-neutral">Step 2</li>
+            <li data-content="✓" class="step step-neutral">Step 3</li>
+            <li data-content="✕" class="step step-neutral">Step 4</li>
+            <li data-content="★" class="step step-neutral">Step 5</li>
+            <li data-content="" class="step step-neutral">Step 6</li>
+            <li data-content="●" class="step step-neutral">Step 7</li>
+        </ul>
+    </div>
 </template>
