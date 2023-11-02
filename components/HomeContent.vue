ab61f4bdb937f9fb7fd21d4002537cc785c67201
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-11-02 20:10:21 +0800

    done navbar and index
    haoxiang take a look

--- components/HomeContent.vue
+++ components/HomeContent.vue
@@ -1,66 +1,137 @@
 <template>
-    <div class="pb-16 max-sm:pb-4">
-      <div class="text-2xl md:text-4xl lg:text-6xl text-white pt-12 max-sm:pt-4 pl-16 md:pl-24 lg:pl-32">
-          <p class="leading-normal font-bold">
-              The <span class="text-orange-500 font-black">first and largest...</span>
-          </p>
-          <div>
-              <p class="leading-normal font-bold">student blockchain club</p>
-              <p class="leading-normal font-bold">in Malaysia</p>
-          </div>
-          <div class="flex py-16 max-sm:py-4">
-            <button class="btn btn-lg max-sm:btn-sm rounded-full border-orange-500 bg-orange-500 hover:bg-orange-400"> 
-                <span class="flex items-center text-white font-bold text-lg max-sm:text-xs">Learn More</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="white" class="h-6 w-6 ml-2 max-sm:h-4 max-sm:w-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
-                </svg>
-            </button>
-
-            <button class="btn btn-lg max-sm:btn-sm rounded-full btn-blue ml-12 border-blue-500 bg-blue-500 hover:bg-blue-400 max-sm:ml-4">
-                <span class="flex items-center text-white font-bold text-lg max-sm:text-xs">Join Now</span>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2 ml-2 max-sm:h-4 max-sm:w-4" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="white">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
-                </svg>
-            </button>
-          </div>
-
-    
+  <div class="pb-16 max-sm:pb-4">
+    <div
+      class="text-2xl md:text-4xl lg:text-6xl text-white pt-12 max-sm:pt-4 px-8 md:px-24 lg:px-32"
+    >
+      <p class="leading-normal font-bold">
+        The <span class="text-orange-500 font-black">first and largest...</span>
+      </p>
+      <div>
+        <p class="leading-normal font-bold">student blockchain club</p>
+        <p class="leading-normal font-bold">in Malaysia</p>
       </div>
-    
-      <div class="w-10/12 flex text-6xl font-Encode text-white ml-32 pt-16 pb-16 pl-16 bg-white bg-opacity-20 backdrop-blur-sm rounded-3xl max-sm:ml-16 max-sm:w-3/4 max-sm:pl-2 max-sm:pt-4 max-sm:pb-4 max-sm:rounded-lg max-sm:mt-2">
-    
-          <div class="flex items-center mx-auto">
-            <svg class="h-28 w-28 max-sm:h-8 max-sm:w-8" fill="white" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/></svg>
-              <div class="pl-2 pr-32 max-sm:pr-2">
-                  <p class="font-bold text-5xl max-sm:text-xs"> 500+ </p>
-                  <p class="font-medium text-4xl max-sm:text-xs"> Members </p>
-              </div>
-          </div>
-    
-          <div class="flex items-center mx-auto">
-              <svg class="h-28 w-28 max-sm:h-8 max-sm:w-8" xmlns="http://www.w3.org/2000/svg" fill="white" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-              <div class="pr-32 max-sm:pr-2">
-                  <p class="font-bold text-5xl max-sm:text-xs"> 50+ </p>
-                  <p class="font-medium text-4xl max-sm:text-xs"> Committees </p>
-              </div>
-          </div>
-    
-          <div class="flex items-center mx-auto">
-              <svg class="w-28 h-28 max-sm:h-8 max-sm:w-8" fill="white" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M438-226 296-368l58-58 84 84 168-168 58 58-226 226ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/></svg>
-              <div class="pr-32 max-sm:pr-2">
-                  <p class="font-bold text-5xl max-sm:text-xs"> 20+ </p>
-                  <p class="font-medium text-4xl max-sm:text-xs"> Events </p>
-              </div>
-          </div>
-      </div>
+      <div class="flex py-16 max-sm:py-4">
+        <button
+          class="btn btn-lg max-sm:btn-sm rounded-full border-orange-500 bg-orange-500 hover:bg-orange-400"
+        >
+          <span
+            class="flex items-center text-white font-bold text-lg max-sm:text-xs"
+            >Learn More</span
+          >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2.5"
+            stroke="white"
+            class="h-6 w-6 ml-2 max-sm:h-4 max-sm:w-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+            />
+          </svg>
+        </button>
 
-      <div class="w-10/12 ml-32 mt-16 pt-12 pb-12 pl-16 pr-16 bg-white bg-opacity-20 backdrop-blur-sm rounded-3xl font-Encode text-center max-sm:ml-16 max-sm:flex-col max-sm:w-3/4 max-sm:text-left max-sm:pt-4 max-sm:pb-4 max-sm:pl-4 max-sm:pr-4 max-sm:mt-4 max-sm:rounded-lg">
-          <h1 class="text-6xl text-orange-500 pb-10 font-bold max-sm:text-xl max-sm:pb-4">Learn, Buidl, Repeat</h1>
-          <p class="text-4xl text-white font-semibold max-sm:text-lg">We are democratizing blockchain and cryptocurrency knowledge through learning and building</p>
+        <button
+          class="btn btn-lg max-sm:btn-sm rounded-full btn-blue ml-12 border-blue-500 bg-blue-500 hover:bg-blue-400 max-sm:ml-4"
+        >
+          <span
+            class="flex items-center text-white font-bold text-lg max-sm:text-xs"
+            >Join Now</span
+          >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 ml-2 ml-2 max-sm:h-4 max-sm:w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2.5"
+            stroke="white"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+            />
+          </svg>
+        </button>
       </div>
-
     </div>
-  
-  
+
+    <div
+      class="mx-32 flex text-6xl font-Encode text-white pt-16 pb-16 bg-white bg-opacity-20 backdrop-blur-sm rounded-3xl max-sm:px-8 max-sm:pt-4 max-sm:mx-8 max-sm:pb-4 max-sm:rounded-lg max-sm:mt-2"
+    >
+      <div class="flex items-center mx-auto">
+        <svg
+          class="h-28 w-28 max-sm:h-8 max-sm:w-8"
+          fill="white"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path
+            d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"
+          />
+        </svg>
+        <div class="pl-2 pr-32 max-sm:pr-2">
+          <p class="font-bold text-5xl max-sm:text-xs">500+</p>
+          <p class="font-medium text-4xl max-sm:text-xs">Members</p>
+        </div>
+      </div>
+
+      <div class="flex items-center mx-auto">
+        <svg
+          class="h-28 w-28 max-sm:h-8 max-sm:w-8"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="white"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path
+            d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"
+          />
+        </svg>
+        <div class="pr-32 max-sm:pr-2">
+          <p class="font-bold text-5xl max-sm:text-xs">50+</p>
+          <p class="font-medium text-4xl max-sm:text-xs">Committees</p>
+        </div>
+      </div>
+
+      <div class="flex items-center mx-auto">
+        <svg
+          class="w-28 h-28 max-sm:h-8 max-sm:w-8"
+          fill="white"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path
+            d="M438-226 296-368l58-58 84 84 168-168 58 58-226 226ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"
+          />
+        </svg>
+        <div class="pr-32 max-sm:pr-2">
+          <p class="font-bold text-5xl max-sm:text-xs">20+</p>
+          <p class="font-medium text-4xl max-sm:text-xs">Events</p>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="mx-32 mt-16 pt-12 pb-12 pl-16 pr-16 bg-white bg-opacity-20 backdrop-blur-sm rounded-3xl font-Encode text-center max-sm:mx-8 max-sm:flex-col max-sm:text-left max-sm:pt-4 max-sm:pb-4 max-sm:pl-4 max-sm:pr-4 max-sm:mt-4 max-sm:rounded-lg"
+    >
+      <h1
+        class="text-6xl text-orange-500 pb-10 font-bold max-sm:text-xl max-sm:pb-4"
+      >
+        Learn, Buidl, Repeat
+      </h1>
+      <p class="text-4xl text-white font-semibold max-sm:text-lg">
+        We are democratizing blockchain and cryptocurrency knowledge through
+        learning and building
+      </p>
+    </div>
+  </div>
 </template>
-  
