0f4bcf82cd2cd872662e04d78fe026e0271cab09
AaronTan11 <tanweihup@gmail.com>
2023-12-04 22:04:39 +0800

    changed the favicon to bcc logo and title

--- app.vue
+++ app.vue
@@ -1,21 +1,30 @@
 <template>
-  
-  <div class="background-container z-0">
-    <NavBar/>
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </div>
-  <Footer/>
+    <div class="background-container z-0">
+        <NavBar />
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
+    <Footer />
 </template>
 
 <style scoped>
-.background-container {
-  background-image: url('./assets/bg.png');
-  background-size: cover;
-  background-position: center;
-  min-height: 100vh;
-}
+    .background-container {
+        background-image: url("./assets/bg.png");
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+    }
 </style>
 
-
+<script setup lang="ts">
+    useHead({
+        title: "APUBCC",
+        meta: [
+            {
+                name: "APU Blockchain & Crypto Club",
+                content: "APU Blockchain & Crypto Club Official Website",
+            },
+        ],
+    });
+</script>
